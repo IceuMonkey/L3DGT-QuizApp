f441a38f5803be8e0cd310790facda6049231fa3
IceuMonkey <cm539067@gmail.com>
2024-05-23 14:54:52 +1200

    first commit

---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import Flask, redirect, url_for, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
