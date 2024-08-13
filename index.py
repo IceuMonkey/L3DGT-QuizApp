562c8c17fa30f50ad7cbaafe2df7b0d7497ad83f
IceuMonkey <cm539067@gmail.com>
2024-08-14 11:32:31 +1200

    TEST COMMIT 14/08

--- index.py
+++ index.py
@@ -1,12 +1,22 @@
 from flask import Flask, redirect, url_for, render_template
+from flask_bycrypt import Bcrypt
 from markupsafe import escape
-
 app = Flask(__name__)
 
+# Basic App route
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# Second App route 
+@app.route("/quizlist")
+def quizlist():
+    return render_template("list.html")
+
+# Dynamic routing using 
+@app.route("/user/<username>") 
+def show_user(username):
+    return f'Hello {username}'
 
 if __name__ == "__main__":
     app.run()
