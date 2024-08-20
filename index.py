434d54ac69b28d05e0b54fd664095f63eefe4c7a
IceuMonkey <cm539067@gmail.com>
2024-08-21 11:35:37 +1200

    SPRINT 2 commit

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 from flask import Flask, redirect, url_for, render_template
-from flask_bycrypt import Bcrypt
 from markupsafe import escape
 app = Flask(__name__)
 
@@ -9,14 +8,19 @@ def index():
     return render_template("index.html")
 
 # Second App route 
-@app.route("/quizlist")
+@app.route("/quiz")
 def quizlist():
-    return render_template("list.html")
+    return render_template("quiz.html")
 
 # Dynamic routing using 
 @app.route("/user/<username>") 
 def show_user(username):
     return f'Hello {username}'
+
+# # Admin routing
+# @app.route("/admin")
+# def admin():
+#     return redirect(url_for("user", name="Admin!"))
 
 if __name__ == "__main__":
     app.run()
