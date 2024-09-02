9052d380932fc3e386e7c553681f05a8ac38bb34
IceuMonkey <cm539067@gmail.com>
2024-09-03 10:30:52 +1200

    SPRINT 3 COMMIT

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from markupsafe import escape
 app = Flask(__name__)
 
@@ -12,10 +12,19 @@ def index():
 def quizlist():
     return render_template("quiz.html")
 
+# GET & POST methods from form
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST": 
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
 # Dynamic routing using 
-@app.route("/user/<username>") 
-def show_user(username):
-    return f'Hello {username}'
+@app.route("/<usr>") 
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 
 if __name__ == "__main__":
